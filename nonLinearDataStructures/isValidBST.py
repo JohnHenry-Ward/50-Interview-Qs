ad8b332a140756126f6497ede4e19bd6c0bdeb2a
JohnHenry-Ward <johnhenry514@gmail.com>
2022-03-11 12:09:46 -0800

    added comments to isValidBST

--- nonLinearDataStructures/isValidBST.py
+++ nonLinearDataStructures/isValidBST.py
@@ -7,16 +7,22 @@ def isValidBSTHelper(node, lowerLimit, upperLimit):
     if node is None:
         return True;
     
-    if node.data <= lowerLimit or node.data >= upperLimit:
+    if node.data <= lowerLimit or node.data >= upperLimit: # if the node is not between the 2 limits, invalid BST
         return False;
     
-    return isValidBSTHelper(node.left, lowerLimit, node.data) and isValidBSTHelper(node.right, node.data, upperLimit);
+    
+    return (
+        # node.left must be between the smallest value above it (which is the parents lower limit) and the parent
+        isValidBSTHelper(node.left, lowerLimit, node.data) 
+        and 
+        # node.right must be between the parent and the largest value above it (which is the parents upper limit)
+        isValidBSTHelper(node.right, node.data, upperLimit));
 
 if __name__ == '__main__':
     root = TreeNode(5);
     node1 = TreeNode(1);
     node2 = TreeNode(7);
-    node3 = TreeNode(3);
+    node3 = TreeNode(6);
     node4 = TreeNode(9);
     node5 = TreeNode(8);
     node6 = TreeNode(10);
